6968c6d441293be999cfee531169a43c69de8923
Victor Martinez <104638472+vsmm-dev@users.noreply.github.com>
2022-10-19 10:22:37 -0500

    Initial Commit

---
+++ i'm rich2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i'm rich2
+//
+//  Created by Victor Martinez on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
